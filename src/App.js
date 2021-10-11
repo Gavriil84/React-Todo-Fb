b913c90ab7af4e1a46620eaa1db7d5a2e1aacdb6
Gavriil84 <gabwt.84@gmail.com>
2021-10-11 15:00:58 -0700

    add buttons and component for todo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import * as React from 'react';
 import { useState } from 'react';
 import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@mui/material';
+import Todo from './Todo';
 
 function App() {
   const [todos, setTodos] = useState(['Take dogs for a walk', 'Take the rubbish out', 'Wash the car']);
   const [input, setInput] = useState('');
 
+  const addTodo = (event) => {
+    // this will fire off when we click the button
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button>Add Todo</button>
-
+      <FormControl>
+        <InputLabel>✅ Write a Todo</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value)}/>
+      </FormControl>
+      <Button disabled={!input} type="submit" onClick={addTodo} variant="contained">Add Todo</Button>
       <ul>
         {todos.map(todo => (
-          <li>{todo}</li>
+          <Todo text={todo} />
         ))}
       </ul>
     </div>
