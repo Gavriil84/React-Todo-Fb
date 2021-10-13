cf9f1d8323d8e8fef742be1489b9d46bc6fa3770
Gavriil84 <gabwt.84@gmail.com>
2021-10-12 22:05:15 -0700

    chage the structure of add and fetch data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { Button, FormControl, Input, InputLabel } from '@mui/material';
 import Todo from './Todo';
 import db from './firebase';
+import firebase from 'firebase';
+
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -12,7 +14,7 @@ function App() {
   //when the app loads, we need to listen to the database and fetch new todos as they get added/removed
   useEffect(() => {
     //this code here.... fires when the app.js loads
-    db.collection('todos').onSnapshot(snapshot => {
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       setTodos(snapshot.docs.map(doc => doc.data().todo))
     })
   }, []);
@@ -21,7 +23,8 @@ function App() {
     // this will fire off when we click the button
     event.preventDefault(); //will stop the refresh
     db.collection('todos').add({
-      todo: input
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
     })
 
     setTodos([...todos, input]);
@@ -31,11 +34,14 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <FormControl>
-        <InputLabel>✅ Write a Todo</InputLabel>
-        <Input value={input} onChange={event => setInput(event.target.value)}/>
-      </FormControl>
-      <Button disabled={!input} type="submit" onClick={addTodo} variant="contained">Add Todo</Button>
+      
+      <form>
+        <FormControl>
+          <InputLabel>✅ Write a Todo</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)}/>
+        </FormControl>
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained">Add Todo</Button>
+      </form>
       <ul>
         {todos.map(todo => (
           <Todo text={todo} />
